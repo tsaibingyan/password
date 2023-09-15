a79b75fe01b0d193722c7152aaf414994e9cbe58
Mike Tsai <miem585852011314@gmail.com>
2023-09-15 19:09:37 +0800

    change to i > 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 chance = 3
-while chance <= 3:
+while chance > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
@@ -8,5 +8,3 @@ while chance <= 3:
 	else:
 		chance = chance-1
 		print('密碼錯誤! 還有 %d 機會' %chance)
-		if chance == 0:
-			break

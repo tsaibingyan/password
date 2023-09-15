cb91aa96106f3bb0f392506206d4fc53adfa44a6
Mike Tsai <miem585852011314@gmail.com>
2023-09-15 19:05:47 +0800

    basic ver.

--- password.py
+++ password.py
@@ -1,14 +1,12 @@
-x = 0
-while x <= 3:
-	password = input('請輸入密碼: ')
-	if password == 'a123456':
+password = 'a123456'
+chance = 3
+while chance <= 3:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
 		print('登入成功')
 		break
-	x = x + 1
-	if x == 1:
-		print('密碼錯誤，還有2次機會')
-	elif x == 2:
-		print('密碼錯誤，還有1次機會')
 	else:
-		print('密碼錯誤，已無機會')
-		break
+		chance = chance-1
+		print('密碼錯誤! 還有 %d 機會' %chance)
+		if chance == 0:
+			break
